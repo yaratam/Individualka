40bde4bbe5b0de590873d4bc545cbcbe16e71ce2
dalie <darknight0417@gmail.vom>
2021-12-08 03:29:40 +0600

    second commit, almost done

---
+++ app/src/main/java/com/example/individualka/Seven.kt
@@ -0,0 +1,11 @@
+package com.example.individualka
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Seven : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_seven)
+    }
+}
