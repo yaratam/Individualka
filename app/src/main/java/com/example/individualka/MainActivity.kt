1836d0ab1dabeee81b3ad4f2ebffed888fc53894
tatarrrrrr <darknight0417@gmail.com>
2021-12-08 20:50:04 +0600

    Third( without perehods)2

--- app/src/main/java/com/example/individualka/MainActivity.kt
+++ app/src/main/java/com/example/individualka/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.individualka
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView = findViewById<ImageView>(R.id.signUp)
+
+        textView.setOnClickListener{
+            startActivity(Intent(this,Two::class.java))
+        }
     }
 }
