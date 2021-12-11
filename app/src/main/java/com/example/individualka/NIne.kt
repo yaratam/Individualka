bed621b3a13b92623e9ab50455bf1b74bd4ae785
guest1 <akm17122003@gmail.com>
2021-12-11 17:21:32 +0600

    Fianal!Final!

--- app/src/main/java/com/example/individualka/NIne.kt
+++ app/src/main/java/com/example/individualka/NIne.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
 
-class NIne : AppCompatActivity() {
+class Nine : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_homemeditate_nine)
